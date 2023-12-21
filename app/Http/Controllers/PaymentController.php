63f4ef27e061ae7894ea55128bb5803a11438ead
Noah Ortega <38738663+NoahOrtega@users.noreply.github.com>
2023-12-21 15:17:52 -0500

    wePay switching to embed

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -1,10 +1,219 @@
 <?php
-
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Throwable;
+use \WePay;
 
 class PaymentController extends Controller
 {
-    public function show() {
-        return view('page.payment');
+    function setPrice($price) {
+        $surcharge = round($price * ((env('WEPAY_SURCHARGE_PERCENTAGE_AS_INT')/ 100.0)), 2);
+        $total = $price + $surcharge;
+
+        session(['subtotal' => $price]);
+        session(['surcharge' => $surcharge]);
+        session(['total' => $total]);
+        session(['payment-ID' => (session()->getId()).'gg'.$price]);
     }
+    function clearPrice() {
+        session(['subtotal' => null]);
+        session(['surcharge' => null]);
+        session(['total' => null]);
+        session(['payment-ID' => null]);
+    }
+
+    //Provides the payment start page. Resets the transaction ID on load
+    public function showPaymentPage()  {
+        $this->clearPrice();
+        return view('page.pay.payment');
+    }
+
+    //On confirmation of price, create a UID based on price and session, save it to the session
+    public function confirmPrice(Request $request) {
+        $price = $request->price;
+        if(is_numeric($price) && $price > 0) {
+            $this->setPrice(round($price, 2));
+            return redirect()->route('checkout');
+        }
+    }
+
+    //Provides the checkout page, loads the UID and clears it from the session
+    public function showCheckoutPage() {
+        $UID = session('payment-ID');
+
+        if($UID == null) {
+            return redirect()->route('pay');
+        }
+
+        return view('page.pay.checkout2')
+        ->with('uid', $UID)
+        ->with('subtotal', session('subtotal'))
+        ->with('surcharge', session('surcharge'))
+        ->with('total', session('total'));
+    }
+
+    function cardTokenToPaymentMethod($request) {
+        $appId = env('WEPAY_CLIENT_ID');
+        $appToken = env('WEPAY_ACCESS_TOKEN');
+        $uniqueKey = session('uid');
+
+
+        $url = 'https://stage-api.wepay.com/payment_methods';
+        //TODO: switch stage to actual api url
+
+        $data = [
+            "type" => "credit_card",
+            "credit_card" => [
+                "card_holder" => [
+                    "holder_name" => $request->name,
+                    "email" => $request->email,
+                    "address" => [
+                        "line1" => $request->line1,
+                        "line2" => $request->line2,
+                        "region" => $request->region,
+                        "postal_code" => $request->postal_code,
+                        "country" => "US"
+                    ]
+                ]
+            ],
+            "token" => [
+                "id" => "".$request->card_token
+            ]
+        ];
+        $jsonData = json_encode($data);
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'App-Id: ' . $appId,
+            'App-Token: ' . $appToken,
+            'Api-Version: 3.0',
+            'Content-Type: application/json',
+            'Unique-Key: ' . $uniqueKey
+        ]);
+
+        // Execute the request
+        $response = curl_exec($ch);
+
+        // Check for errors
+        if ($response === false) {
+            $error = curl_error($ch);
+            curl_close($ch);
+            $return = 'cURL Error: ' . $error;
+        } else {
+            // Handle the response
+            curl_close($ch);
+            $return = 'Response: ' . $response;
+        }
+        //TODO:clear session variables if successful
+        return(json_encode($return));
+    }
+
+    //Submits payment token via WePay api call,
+    //display success or failure message. email solcar and the user that a transaction has been made
+    public function confirmCheckout(Request $request) {
+
+        $account_id = env('WEPAY_ACCOUNT_ID');
+        $credit_card_id = $request->card_id;
+        $b = $this->cardTokenToPaymentMethod($request);
+        return($b);
+
+        // API endpoint
+        $url = 'https://stage-api.wepay.com/payments';
+        //TODO: switch stage to actual api url
+
+        // Your App's credentials
+        $appId = env('WEPAY_CLIENT_ID');
+        $appToken = env('WEPAY_ACCESS_TOKEN');
+
+        $uniqueKey = session('uid');
+        $amount = session('total')*100;
+
+        // Data to send in the request (sample JSON data)
+        $data = [
+            "account_id" => $account_id,
+            "amount" => $amount,
+            "currency" => "USD",
+            "payment_method" => [
+                "token" => [
+                    "id" => ''.$credit_card_id
+                ],
+                "credit_card" => [
+                    "card_holder" => [
+                        "holder_name" => "Test Test",
+                        "email" => "foo@bar.com",
+                        "address" => [
+                            "country" => "US",
+                            "postal_code" => "94025"
+                        ]
+                    ]
+                ]
+            ],
+        ];
+        $jsonData = json_encode($data);
+        //TODO: Replace credit_card info with real data from the form and supply risk bits
+
+        // cURL initialization
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'App-Id: ' . $appId,
+            'App-Token: ' . $appToken,
+            'Api-Version: 3.0',
+            'Content-Type: application/json',
+            'Unique-Key: ' . $uniqueKey
+        ]);
+
+        // Execute the request
+        $response = curl_exec($ch);
+
+        // Check for errors
+        if ($response === false) {
+            $error = curl_error($ch);
+            curl_close($ch);
+            $return = 'cURL Error: ' . $error;
+        } else {
+            // Handle the response
+            curl_close($ch);
+            $return = 'Response: ' . $response;
+        }
+        //TODO:clear session variables if successful
+        return(json_encode($return));
+
+        // $env_type = env('WEPAY_ENVIRONMENT_TYPE');
+
+        // Wepay::useStaging($client_id, $client_secret);
+
+        // $wepay = new WePay($access_token);
+        // try {
+        //     $response = $wepay->request('checkout/create', array(
+        //         'account_id'          => $account_id,
+        //         'amount'              => '25.50',
+        //         'currency'            => 'USD',
+        //         'short_description'   => 'electrical contracting bill',
+        //         'type'                => 'service',
+        //         'payment_method'      =>
+        //             array(
+        //                 'type'        => 'credit_card',
+        //                 'credit_card' =>
+        //                     array(
+        //                         'id'  => $credit_card_id
+        //             )
+        //         )
+        //     ));
+        // }
+        // catch(Throwable $e) {
+        //     return(json_encode($e));
+        // }
+
+        //make API call
+        // return(null);
+    }
+
 }
---
+++ resources/js/wepayFunctions.js
@@ -0,0 +1,195 @@
+(function () {
+    WePay.set_endpoint(environmentType); // change to "production" when live
+
+    // Shortcuts
+    var d = document;
+    (d.id = d.getElementById),
+        (valueById = function (id) {
+            return d.id(id).value;
+        });
+    // For those not using DOM libraries
+    var addEvent = function (e, v, f) {
+        if (!!window.attachEvent) {
+            e.attachEvent("on" + v, f);
+        } else {
+            e.addEventListener(v, f, false);
+        }
+    };
+
+    embedWePay();
+
+    // Attach the event to the DOM
+    addEvent(d.id("submit-button"), "click", function () {
+        const name = valueById("firstname") + " " + valueById("lastname");
+        const email = valueById("email");
+        const line1 = valueById("street");
+        const line2 = valueById("apartment");
+        const region = valueById("state");
+        const city = valueById("city");
+        const postal_code = valueById("zip");
+
+        response = WePay.credit_card.create(
+            {
+                client_id: myAppId,
+                user_name: name,
+                email: email,
+                cc_number: valueById("cardnumber"),
+                cvv: valueById("securitycode"),
+                expiration_month: valueById("expirationdate").substr(0, 2),
+                expiration_year: valueById("expirationdate").substr(-2, 2),
+                address: {
+                    postal_code: postal_code,
+                    city: city,
+                    region: region,
+                },
+            },
+            function (data) {
+                if (data.error) {
+                    console.log(data);
+
+                    if (data.error_code == 1003) {
+                        console.log("Invalid card number");
+                    }
+                    //TODO: handle more form errors
+                } else {
+                    console.log(data);
+                    // call your own app's API to save the token inside the data;
+
+                    const formData = {
+                        name: name,
+                        email: email,
+                        line1: line1,
+                        line2: line2,
+                        region: region,
+                        city: city,
+                        postal_code: postal_code,
+                        card_id: data.credit_card_id,
+                    };
+
+                    confirmPayment(formData);
+                }
+            }
+        );
+    });
+
+    function embedWePay() {
+        var options = {
+            custom_style: custom_style,
+            show_labels: true,
+            show_placeholders: true,
+            show_error_messages: true,
+            show_error_messages_when_unfocused: true,
+            use_one_liner: false,
+        };
+
+        // credit card iframe styling
+        var custom_style = {
+            styles: {
+                base: {
+                    color: "grey",
+                    border: "1px solid grey",
+                    "border-top": "none",
+                    "border-right": "none",
+                    "border-left": "none",
+                    "font-weight": "200",
+                    "font-family": "Arial",
+                    padding: "0px",
+                    "margin-bottom": "5px",
+                    ":focus": {
+                        border: "2px solid #4db6ac",
+                        "border-top": "none",
+                        "border-right": "none",
+                        "border-left": "none",
+                    },
+                    "::placeholder": {
+                        "text-transform": "lowercase",
+                        color: "#D3D3D3",
+                        "font-size": "17px",
+                    },
+                },
+                invalid: {
+                    color: "#CD5C5C",
+                    "border-color": "#CD5C5C",
+                },
+                valid: {
+                    color: "#4db6ac",
+                    "border-color": "#4db6ac",
+                },
+                labels: {
+                    base: {
+                        color: "gray",
+                        "font-family": "Arial",
+                        "font-size": "13px",
+                        "font-weight": "1",
+                        "text-transform": "lowercase",
+                        padding: "0px",
+                        "padding-left": "0px",
+                    },
+                },
+                errors: {
+                    invalid: {
+                        color: "#CD5C5C",
+                    },
+                },
+            },
+        };
+
+        if (options.use_one_liner) {
+            custom_style = {
+                styles: {
+                    "cc-logo": {
+                        base: {
+                            margin: "12px 12px 7px 12px",
+                        },
+                    },
+                    "cc-mask": {
+                        base: {
+                            margin: "12px 12px 7px 12px",
+                        },
+                    },
+                    "cvv-icon": {
+                        base: {
+                            display: "none",
+                        },
+                    },
+                },
+            };
+        }
+
+        //credit card iframe configs
+        var error = WePay.configure("stage", myAppId, apiVersion);
+        if (error) {
+            console.log(error);
+        }
+
+        var iframe_container_id = "credit_card_iframe";
+        var creditCard = WePay.createCreditCardIframe(iframe_container_id, options);
+
+    }
+
+    function confirmPayment(formData) {
+        const url = "/pay/checkout/checkout";
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": myCSRF,
+            },
+            body: JSON.stringify(formData),
+        };
+
+        fetch(url, requestOptions)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(response);
+                }
+                return response.json(); // Parse response body as JSON
+            })
+            .then((data) => {
+                console.log("Confirmation response:", data);
+            })
+            .catch((error) => {
+                console.error("Unable to confirm payment", error);
+            });
+    }
+})();
